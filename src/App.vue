6baf6e123af63dabe1f5a51424074c0d36d1e6db
Kirill <pesterevkv2003@gmail.com>
2024-05-06 20:02:45 +0300

    upd: try to create requests on server

--- src/App.vue
+++ src/App.vue
@@ -2,22 +2,42 @@
 import TheHeader from './components/TheHeader.vue'
 import TheSearch from './components/TheSearch.vue'
 import TheInformation from './components/TheInformation.vue'
-import { ref } from 'vue'
+import axios from 'axios'
+import { onMounted, reactive, ref } from 'vue'
 
-const items = ref([])
+// const items = ref([])
+// const filters = reactive({
+//   sortBy: 'name',
+//   search: ''
+// })
 
-const getItems = async () => {
-  const response = await fetch('https://jsonplaceholder.typicode.com/users')
-  const data = await response.json()
-  items.value = data
-}
+// const onChangeSearchInput = (e) => {
+//   filters.search = e.target.value
+// }
+// const fetchItems = async () => {
+//   try {
+//     const params = {
+//       sortBy: filters.sortBy
+//     }
+//     if (filters.search) {
+//       params.title = `*${filters.search}*`
+//     }
+//     const { data } = await axios.get('https://jsonplaceholder.typicode.com/users', { params })
+//     items.value = data
+//   } catch (error) {
+//     console.log(error)
+//   }
+// }
+// onMounted(() => {
+//   fetchItems()
+// })
 </script>
 
 <template>
   <div class="app">
     <TheHeader />
     <div class="main">
-      <TheSearch />
+      <TheSearch :onChangeSearchInput="onChangeSearchInput" />
       <TheInformation />
     </div>
   </div>
--- src/components/TheSearch.vue
+++ src/components/TheSearch.vue
@@ -1,12 +1,20 @@
 <script setup>
 import EmployeeCard from './EmployeeCard.vue'
+
+const emit = defineEmits(['createOrder'])
+const model = defineModel()
+
+defineProps({
+  onChangeSearchInput: Function
+})
 </script>
 
 <template>
   <div class="search">
     <div class="find">
       <h3>Поиск сотрудников</h3>
-      <input class="find__input" type="text" placeholder="Введите id или имя" />
+      <input v-model="model" class="find__input" type="text" placeholder="Введите id или имя" />
+      <button @click="() => emit('createOrder')" class="find__button">Найти</button>
     </div>
     <div class="result">
       <h3>Результаты</h3>
